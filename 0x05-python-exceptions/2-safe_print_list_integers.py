4f2efc4169b601a34315d392b8728c6fafeb3a62
Lavender-munyasa <lavendermunyasa23@gmail.com>
2022-12-21 13:55:37 -0800

    python exceptions exercises

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+afe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for index in range(x):
+        try:
+            print('{:d}'.format(my_list[index]), end='')
+            count += 1
+        except (ValueError, TypeError):
+            continue
+
+    print()
+    return 
